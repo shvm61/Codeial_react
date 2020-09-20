d75e94e933f1c34d528fb686f3d99341b5a48c04
shvm61 <60419370+shvm61@users.noreply.github.com>
2020-09-20 10:07:31 +0530

    navbar

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./components/App";
-import { configureStore } from "./store";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import './index.css';
+import App from './components/App';
+import { configureStore } from './store';
 
 const store = configureStore();
-console.log("store", store.getState());
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
+  document.getElementById('root')
 );
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,12 @@
-import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
-import logger from "redux-logger";
-import reducers from "../reducers";
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import reducer from '../reducers';
+
 let store;
+
 export function configureStore() {
-  store = createStore(reducers, applyMiddleware(thunk, logger));
+  store = createStore(reducer, applyMiddleware(thunk, logger));
+
   return store;
 }
